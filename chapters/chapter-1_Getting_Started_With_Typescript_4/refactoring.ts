ab25a0e663ec3fd0458ead8e9ccba25e724ce635
Alejo Boga <alejoboga20@gmail.com>
2023-02-02 15:25:46 -0500

    refactor with vs code

--- chapters/chapter-1_Getting_Started_With_Typescript_4/refactoring.ts
+++ chapters/chapter-1_Getting_Started_With_Typescript_4/refactoring.ts
@@ -1,17 +1,19 @@
-function find<T>(arr: T[], predicate: (item: T) => boolean) {
-  for (let item of arr) {
-    if (predicate(item)) {
-      return item;
-    }
-  }
-  return undefined;
+type Predicate<T> = (item: T) => boolean;
+
+function find<T>(arr: T[], predicate: Predicate<T>) {
+	for (let item of arr) {
+		if (predicate(item)) {
+			return item;
+		}
+	}
+	return undefined;
 }
 
 function indexOf<T>(arr: T[], predicate: (item: T) => boolean) {
-  for (let i = 0; i < arr.length; i += 1) {
-    if (predicate(arr[i])) {
-      return i;
-    }
-  }
-  return -1;
+	for (let i = 0; i < arr.length; i += 1) {
+		if (predicate(arr[i])) {
+			return i;
+		}
+	}
+	return -1;
 }
